c3fa6232f6868bb8b7a71a99d6414ad5c003b377
Fabian Petersen <fabian@nf-petersen.de>
2025-02-05 14:06:23 +0100

    nvim: put plugins into own folder and add keymaps file

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps for clipboard interaction
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>p', [["+p]])
+vim.keymap.set('v', '<leader>P', [["_dP]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+
+-- Extra save commands because of fat-finger
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Wa', 'wa', {})
+vim.api.nvim_create_user_command('Wqa', 'wqa', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
+
+-- kubernetes secrets
+vim.keymap.set('n', 'sen', "<cmd>%!yq -e '.data = (.data | withEntries(.value = (.value | @base64)))'<cr>", { desc = 'Encrypt k8s base64 keys' })
+
+print 'Hello'
+
+vim.keymap.set('n', 'sde', "<cmd>%!yq -e '.data = (.data | withEntries(.value = (.value | @base64d)))'<cr>", { desc = 'Decrypt k8s base64 keys' })
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,43 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+			skip_confirm_for_simple_edits = true,
+			lsp_file_methods = {
+				-- Enable or disable LSP file operations
+				enabled = true,
+				-- Time to wait for LSP file operations to complete before skipping
+				timeout_ms = 1000,
+				-- Set to true to autosave buffers that are updated with LSP willRenameFiles
+				-- Set to "unmodified" to only save unmodified buffers
+				autosave_changes = false,
+			},
+			watch_for_changes = true,
+			keymaps = {
+				["g?"] = { "actions.show_help", mode = "n" },
+				["<CR>"] = "actions.select",
+				["<C-s>"] = { "actions.select", opts = { vertical = true } },
+				["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+				["<C-t>"] = { "actions.select", opts = { tab = true } },
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = { "actions.close", mode = "n" },
+				["<C-l>"] = "actions.refresh",
+				["-"] = { "actions.parent", mode = "n" },
+				["_"] = { "actions.open_cwd", mode = "n" },
+				["`"] = { "actions.cd", mode = "n" },
+				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+				["gs"] = { "actions.change_sort", mode = "n" },
+				["gx"] = "actions.open_external",
+				["g."] = { "actions.toggle_hidden", mode = "n" },
+				["g\\"] = { "actions.toggle_trash", mode = "n" },
+			},
+		})
+		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+	end,
+}
