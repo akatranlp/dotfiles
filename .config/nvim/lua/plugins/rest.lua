fbc7416021bbb34696e1d7efdbee4e7e396130f0
Fabian Petersen <fabian@nf-petersen.de>
2026-02-06 13:11:26 +0100

    nvim: require mini.align

--- .config/nvim/lua/plugins/rest.lua
+++ .config/nvim/lua/plugins/rest.lua
@@ -354,6 +354,8 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.align').setup()
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
